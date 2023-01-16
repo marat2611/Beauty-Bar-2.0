6afc4e8e33956ecb15d115c67d21e670832959b7
konnovazhanna <konnovazhanna@gmail.com>
2023-01-16 09:48:13 +0300

    Отправка формы Страница Курса

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+$userName = $_POST['userName'];
+$tel = $_POST['tel'];
+$userName = htmlspecialchars($userName);
+$tel = htmlspecialchars($tel);
+$userName = urldecode($userName);
+$tel = urldecode($tel);
+$userName = trim($userName);
+$tel = trim($tel);
+//echo $userName;
+//echo "<br>";
+//echo $tel;
+if (mail("admin@beautyandschool.ru", "Заявка с сайта", "Имя:".$userName.". E-mail: ".$tel ,"From: admin@beautyandschool.ru \r\n"))
+ {     echo "Сообщение успешно отправлено";
+} else {
+    echo "При отправке сообщения возникли ошибки";
+}?>
